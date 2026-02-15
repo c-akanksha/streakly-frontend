e7c6eb78d4e8de20250ad3f8f6ce1b759e3c5fc0
Akanksha C <akankshaprajwalbhat@gmail.com>
2026-02-15 21:14:48 +0530

    chore: added utils

---
+++ src/utils/icons.jsx
@@ -0,0 +1,91 @@
+import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import BedtimeIcon from "@mui/icons-material/Bedtime";
+import LocalDrinkIcon from "@mui/icons-material/LocalDrink";
+import SelfImprovementIcon from "@mui/icons-material/SelfImprovement";
+import BookIcon from "@mui/icons-material/Book";
+import BrushIcon from "@mui/icons-material/Brush";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import CodeIcon from "@mui/icons-material/Code";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import CleaningServicesIcon from "@mui/icons-material/CleaningServices";
+import MoodIcon from "@mui/icons-material/Mood";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
+import LanguageIcon from "@mui/icons-material/Language";
+import SchoolIcon from "@mui/icons-material/School";
+import NaturePeopleIcon from "@mui/icons-material/NaturePeople";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import VolunteerActivismIcon from "@mui/icons-material/VolunteerActivism";
+import SportsBasketballIcon from "@mui/icons-material/SportsBasketball";
+import DirectionsBikeIcon from "@mui/icons-material/DirectionsBike";
+import HikingIcon from "@mui/icons-material/OutdoorGrill";
+import SpaIcon from "@mui/icons-material/Spa";
+import EmojiPeopleIcon from "@mui/icons-material/EmojiPeople";
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import PetsIcon from "@mui/icons-material/Pets";
+import PoolIcon from "@mui/icons-material/Pool";
+import CoffeeIcon from "@mui/icons-material/LocalCafe";
+import FitnessCenterTwoToneIcon from "@mui/icons-material/FitnessCenter";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
+import LandscapeIcon from "@mui/icons-material/Landscape";
+import SportsTennisIcon from "@mui/icons-material/SportsTennis";
+import SportsHandballIcon from "@mui/icons-material/SportsHandball";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
+import BeachAccessIcon from "@mui/icons-material/BeachAccess";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import SportsGolfIcon from "@mui/icons-material/SportsGolf";
+import IcecreamIcon from "@mui/icons-material/Icecream";
+import SchoolIconOutlined from "@mui/icons-material/SchoolOutlined";
+import SportsCricketIcon from "@mui/icons-material/SportsCricket";
+import SportsRugbyIcon from "@mui/icons-material/SportsRugby";
+import FitnessCenterRoundedIcon from "@mui/icons-material/FitnessCenterRounded";
+
+export const ICON_OPTIONS = [
+  { label: "Running", icon: <DirectionsRunIcon /> },
+  { label: "Workout", icon: <FitnessCenterIcon /> },
+  { label: "Sleep", icon: <BedtimeIcon /> },
+  { label: "Hydration", icon: <LocalDrinkIcon /> },
+  { label: "Meditation", icon: <SelfImprovementIcon /> },
+  { label: "Reading", icon: <BookIcon /> },
+  { label: "Writing", icon: <BrushIcon /> },
+  { label: "Music", icon: <MusicNoteIcon /> },
+  { label: "Coding", icon: <CodeIcon /> },
+  { label: "Cooking", icon: <RestaurantIcon /> },
+  { label: "Cleaning", icon: <CleaningServicesIcon /> },
+  { label: "Mood Tracking", icon: <MoodIcon /> },
+  { label: "Photography", icon: <PhotoCameraIcon /> },
+  { label: "Soccer", icon: <SportsSoccerIcon /> },
+  { label: "Language Learning", icon: <LanguageIcon /> },
+  { label: "Study", icon: <SchoolIcon /> },
+  { label: "Nature Walks", icon: <NaturePeopleIcon /> },
+  { label: "Journaling", icon: <MenuBookIcon /> },
+  { label: "Volunteer", icon: <VolunteerActivismIcon /> },
+  { label: "Basketball", icon: <SportsBasketballIcon /> },
+  { label: "Cycling", icon: <DirectionsBikeIcon /> },
+  { label: "Hiking", icon: <HikingIcon /> },
+  { label: "Spa/Relax", icon: <SpaIcon /> },
+  { label: "Social Time", icon: <EmojiPeopleIcon /> },
+  { label: "Travel", icon: <TravelExploreIcon /> },
+  { label: "Pet Care", icon: <PetsIcon /> },
+  { label: "Swimming", icon: <PoolIcon /> },
+  { label: "Coffee Break", icon: <CoffeeIcon /> },
+  { label: "Gym", icon: <FitnessCenterTwoToneIcon /> },
+  { label: "Library Visit", icon: <LocalLibraryIcon /> },
+  { label: "Gardening", icon: <LandscapeIcon /> },
+  { label: "Tennis", icon: <SportsTennisIcon /> },
+  { label: "Handball", icon: <SportsHandballIcon /> },
+  { label: "Gaming", icon: <SportsEsportsIcon /> },
+  { label: "Florist/Plant Care", icon: <LocalFloristIcon /> },
+  { label: "Beach Day", icon: <BeachAccessIcon /> },
+  { label: "Shopping", icon: <LocalMallIcon /> },
+  { label: "Driving Practice", icon: <DirectionsCarIcon /> },
+  { label: "Golf", icon: <SportsGolfIcon /> },
+  { label: "Ice Cream Treat", icon: <IcecreamIcon /> },
+  { label: "School Homework", icon: <SchoolIconOutlined /> },
+  { label: "Cricket", icon: <SportsCricketIcon /> },
+  { label: "Rugby", icon: <SportsRugbyIcon /> },
+  { label: "Fitness (Rounded)", icon: <FitnessCenterRoundedIcon /> },
+];
---
+++ src/utils/logout.js
@@ -0,0 +1,11 @@
+import { logout } from "../utils/logout";
+
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      logout();
+    }
+    return Promise.reject(error);
+  },
+);
